444db37194ce5c6ae96421fb5d9c5529a4c49823
ycrad <just4steven@gmail.com>
2024-02-18 21:51:49 +0800

    Fix docker image name.

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
 
-# 判断本地是否存在docker镜像stellauto:latest
-if [ -z "$(docker images -q stellauto:latest 2> /dev/null)" ]; then
+# 判断本地是否存在docker镜像ycrad/stellauto:latest
+if [ -z "$(docker images -q ycrad/stellauto:latest 2> /dev/null)" ]; then
   # 不存在镜像，则拉取该镜像
-  echo "不存在镜像stellauto:latest，正在拉取..."
-  docker pull ycrad/stellauto:1.0.0
+  echo "不存在镜像ycrad/stellauto:latest，正在拉取..."
+  docker pull ycrad/stellauto:latest
 fi
 
 # 判断本地是否存在使用该镜像的容器
-if [ -z "$(docker ps -a --filter ancestor=stellauto:latest --format '{{.ID}}')" ]; then
+if [ -z "$(docker ps -a --filter ancestor=ycrad/stellauto:latest --format '{{.ID}}')" ]; then
   # 不存在容器，则新建一个名为stellauto的容器
-  echo "不存在使用镜像stellauto:latest的容器，正在新建..."
-  docker run -it -d -u stella -p 23459:23459 --name stellauto stellauto:latest
+  echo "不存在使用镜像ycrad/stellauto:latest的容器，正在新建..."
+  docker run -it -d -u stella -p 23459:23459 --name stellauto ycrad/stellauto:latest
 fi
 
 # 获取容器的ID
-container_id=$(docker ps -a --filter ancestor=stellauto:latest --format '{{.ID}}')
+container_id=$(docker ps -a --filter ancestor=ycrad/stellauto:latest --format '{{.ID}}')
 # 判断容器是否在运行
 if [ "$(docker inspect -f '{{.State.Running}}' $container_id)" = "true" ]; then
   # 容器在运行，则直接启动该容器
